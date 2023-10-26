8c9a8ed819008dbd68e8e0395ce8b51714872bc1
sohyuneeee <sohyuney@naver.com>
2023-10-26 19:26:27 +0900

    feat #12: securityConfig에 jwt 관련 설정 추가

--- src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
@@ -1,33 +1,66 @@
 package com.wanted.teamr.snsfeedintegration.security;
 
+import com.wanted.teamr.snsfeedintegration.jwt.JwtAccessDeniedHandler;
+import com.wanted.teamr.snsfeedintegration.jwt.JwtAuthenticationEntryPoint;
+import com.wanted.teamr.snsfeedintegration.jwt.TokenProvider;
+import com.wanted.teamr.snsfeedintegration.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Value("${jwt.secret}")
+    String SECRET_KEY;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
     @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(HttpBasicConfigurer::disable)
+                .exceptionHandling(authenticationManager -> authenticationManager
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler));
+
         http
-            .authorizeHttpRequests(customizer -> customizer
-                .requestMatchers(HttpMethod.POST, "/api/members/*")
-                    .permitAll()
-                .anyRequest()
-                    .authenticated())
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .csrf(AbstractHttpConfigurer::disable)
-            .sessionManagement(customizer -> customizer
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .authorizeHttpRequests(customizer -> customizer
+                        .requestMatchers(HttpMethod.POST, "/api/members/*")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(customizer -> customizer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        /* JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용 */
+        http.apply(new JwtSecurityConfig(SECRET_KEY, tokenProvider, userDetailsService));
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
     }
 
 }
