41a0409ba85a07e6968e61ce2fd216638e9f2775
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-26 17:42:40 +0900

    feat #11: 사용자 회원가입 요청 DTO 정의
    검증 순서를 위한 유틸 인터페이스 정의
    메시지소스 메시지 정의

---
+++ src/main/java/com/wanted/teamr/snsfeedintegration/dto/JoinRequestDTO.java
@@ -0,0 +1,26 @@
+package com.wanted.teamr.snsfeedintegration.dto;
+
+import com.wanted.teamr.snsfeedintegration.util.ValidationGroup;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class JoinRequestDTO {
+
+    @NotBlank
+    private String accountName;
+
+    @NotBlank(groups = ValidationGroup.NotBlank.class)
+    @Email(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$",
+            groups = ValidationGroup.Email.class)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
