144595a437834e91f08584efad34a2cf2cd074cf
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-26 18:01:42 +0900

    feat #11: 사용자 회원가입 컨트롤러 구현

--- src/main/java/com/wanted/teamr/snsfeedintegration/controller/MemberController.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/controller/MemberController.java
@@ -1,7 +1,27 @@
 package com.wanted.teamr.snsfeedintegration.controller;
 
+import com.wanted.teamr.snsfeedintegration.dto.JoinRequestDTO;
+import com.wanted.teamr.snsfeedintegration.service.MemberService;
+import com.wanted.teamr.snsfeedintegration.util.ValidationSequence;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
 @RestController
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@Validated(ValidationSequence.class) @RequestBody JoinRequestDTO dto) {
+        memberService.join(dto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
