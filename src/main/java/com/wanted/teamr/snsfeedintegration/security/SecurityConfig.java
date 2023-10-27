4437a37de70909bb6ee21ea955d7064f019154dd
sohyuneeee <sohyuney@naver.com>
2023-10-27 11:18:12 +0900

    style #12: SecurityConfig에 jwt 관련 재설정

--- src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
@@ -6,15 +6,13 @@ import com.wanted.teamr.snsfeedintegration.jwt.TokenProvider;
 import com.wanted.teamr.snsfeedintegration.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,25 +30,29 @@ public class SecurityConfig {
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(HttpBasicConfigurer::disable)
-                .exceptionHandling(authenticationManager -> authenticationManager
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                        .accessDeniedHandler(jwtAccessDeniedHandler));
-
         http
-                .authorizeHttpRequests(customizer -> customizer
-                        .requestMatchers(HttpMethod.POST, "/api/members/*")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(customizer -> customizer
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                )
+                // enable h2-console
+                .headers((headers)->
+                        headers.contentTypeOptions(contentTypeOptionsConfig ->
+                                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)))
+                // disable session
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
 
+                .authorizeHttpRequests((authorizeRequests)->
+                        authorizeRequests
+                                .requestMatchers("/api/members/**").permitAll()
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .anyRequest().authenticated()
+                );
 
         /* JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용 */
         http.apply(new JwtSecurityConfig(SECRET_KEY, tokenProvider, userDetailsService));
