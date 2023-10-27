9636c5f69e475b05b21a259320b5225d60f28a52
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-27 18:16:27 +0900

    test #11: 회원가입 서비스 성공

--- src/test/java/com/wanted/teamr/snsfeedintegration/service/MemberServiceTest.java
+++ src/test/java/com/wanted/teamr/snsfeedintegration/service/MemberServiceTest.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class MemberServiceTest {
 
@@ -44,6 +46,24 @@ class MemberServiceTest {
             Assertions.assertThatThrownBy(() -> sut.join(dto))
                     .isInstanceOf(CustomException.class)
                     .hasMessage(ErrorCode.DUPLICATE_ACCOUNT_NAME.getMessage());
+        }
+
+        @DisplayName("성공")
+        @Test
+        void givenNotDuplicateAccountName_thenSuccess() {
+            // given
+            MemberJoinRequest dto = new MemberJoinRequest("jeonggoo75", "jeonggoo75@gmail.com", "qlalfqjsgh486^^");
+
+            // when
+            Long memberId = sut.join(dto);
+
+            // then
+            Member foundMember = memberRepository.findById(memberId).orElseThrow();
+            assertThat(foundMember.getAccountName()).isEqualTo(dto.getAccountName());
+            assertThat(foundMember.getEmail()).isEqualTo(dto.getEmail());
+            assertThat(passwordEncoder.matches(dto.getPassword(), foundMember.getPassword())).isTrue();
+            assertThat(foundMember.getApprovalCode()).hasSize(6);
+            assertThat(foundMember.getIsApproved()).isFalse();
         }
 
     }
