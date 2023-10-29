76879fea78e54794b3dc5df9059edbf9b821b7c2
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-29 18:14:37 +0900

    test #22: 가입승인 성공 및 이미 승인된 경우 통합 테스트

--- src/test/java/com/wanted/teamr/snsfeedintegration/controller/MemberControllerTest.java
+++ src/test/java/com/wanted/teamr/snsfeedintegration/controller/MemberControllerTest.java
@@ -1,8 +1,11 @@
 package com.wanted.teamr.snsfeedintegration.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wanted.teamr.snsfeedintegration.domain.Member;
+import com.wanted.teamr.snsfeedintegration.dto.MemberApprovalRequest;
 import com.wanted.teamr.snsfeedintegration.dto.MemberJoinRequest;
 import com.wanted.teamr.snsfeedintegration.exception.ErrorCode;
+import com.wanted.teamr.snsfeedintegration.repository.MemberRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 @DisplayName("/api/members 통합 테스트")
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -27,9 +31,12 @@ class MemberControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper mapper;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @DisplayName("/join 사용자 회원가입")
+    @Order(1)
     @Nested
     class Join {
 
@@ -67,4 +74,55 @@ class MemberControllerTest {
         }
 
     }
+
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    @DisplayName("/approve 사용자 가입승인")
+    @Order(2)
+    @Nested
+    class Approve {
+
+        private static final String URI = BASE_URI + "/approve";
+
+        private String approvalCode;
+
+        @BeforeEach
+        void setUp() {
+            Member member = memberRepository.findById(1L).orElseThrow();
+            approvalCode = member.getApprovalCode();
+        }
+
+        @Order(1)
+        @DisplayName("성공")
+        @Test
+        void whenSuccess_then201() throws Exception {
+            MemberApprovalRequest dto = MemberApprovalRequest.of(ACCOUNT_NAME, PASSWORD, approvalCode);
+            String content = mapper.writeValueAsString(dto);
+            mockMvc.perform(post(URI)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(content))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$").doesNotExist())
+                    .andDo(print())
+                    .andReturn();
+        }
+
+        @Order(2)
+        @DisplayName("이미 가입승인 완료")
+        @Test
+        void givenAlreadyApproved_then400() throws Exception {
+            MemberApprovalRequest dto = MemberApprovalRequest.of(ACCOUNT_NAME, PASSWORD, approvalCode);
+            String content = mapper.writeValueAsString(dto);
+            ErrorCode errorCode = ErrorCode.ALREADY_APPROVED;
+            mockMvc.perform(post(URI)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(content))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.errorCode").value(errorCode.name()))
+                    .andExpect(jsonPath("$.message").value(errorCode.getMessage()))
+                    .andDo(print())
+                    .andReturn();
+        }
+
+    }
+
 }
