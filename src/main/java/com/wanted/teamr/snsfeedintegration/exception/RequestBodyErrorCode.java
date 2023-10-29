7e8084e9b8f21ba14cf73231b765e0c007426954
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-29 15:42:19 +0900

    feat #22: 빈 승인코드 에러코드 정의

--- src/main/java/com/wanted/teamr/snsfeedintegration/exception/RequestBodyErrorCode.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/exception/RequestBodyErrorCode.java
@@ -11,6 +11,7 @@ public enum RequestBodyErrorCode implements ErrorCodeType {
     ACCOUNT_NAME_BLANK("계정 이름은 공백일 수 없습니다.", HttpStatus.BAD_REQUEST, "NotBlank.accountName"),
     EMAIL_BLANK("이메일은 공백일 수 없습니다.", HttpStatus.BAD_REQUEST, "NotBlank.email"),
     PASSWORD_BLANK("비밀번호는 공백일 수 없습니다.", HttpStatus.BAD_REQUEST, "NotBlank.password"),
+    APPROVAL_CODE_BLANK("승인코드는 공백일 수 없습니다.", HttpStatus.BAD_REQUEST, "NotBlank.approvalCode"),
     EMAIL_INVALID_FORMAT("이메일 형식이 유효하지 않습니다.", HttpStatus.BAD_REQUEST, "Email.email"),
     ;
 
