6c9ae5341ff8d9eeedfeb88d46ee667c1afc0961
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-29 16:58:16 +0900

    feat #22: 계정이름으로 회원 찾기 dao 메소드 정의

--- src/main/java/com/wanted/teamr/snsfeedintegration/repository/MemberRepository.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.wanted.teamr.snsfeedintegration.repository;
 import com.wanted.teamr.snsfeedintegration.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByAccountName(String accountName);
 }
