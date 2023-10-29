0bb97dce432a884506b5d2cac00c97666f1e1b77
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-29 10:48:03 +0900

    refactor #22: `ValidationGroup` 대신 기존 애너테이션 클래스 활용
    굳이 새로 만드는 것이 불필요한 것 같아서 제거함

--- src/main/java/com/wanted/teamr/snsfeedintegration/util/ValidationSequence.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/util/ValidationSequence.java
@@ -1,8 +1,13 @@
 package com.wanted.teamr.snsfeedintegration.util;
 
 import jakarta.validation.GroupSequence;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.groups.Default;
 
-@GroupSequence({Default.class, ValidationGroup.NotBlank.class, ValidationGroup.Email.class})
+@GroupSequence({
+        Default.class,
+        NotBlank.class,
+        Email.class})
 public interface ValidationSequence {
 }
