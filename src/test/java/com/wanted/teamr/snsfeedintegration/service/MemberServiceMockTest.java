4865f97e0656c4af4975e41861c28a5445101f8d
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-29 16:56:10 +0900

    test #22: 가입승인 서비스 비밀번호 불일치하면 예외

--- src/test/java/com/wanted/teamr/snsfeedintegration/service/MemberServiceMockTest.java
+++ src/test/java/com/wanted/teamr/snsfeedintegration/service/MemberServiceMockTest.java
@@ -1,5 +1,6 @@
 package com.wanted.teamr.snsfeedintegration.service;
 
+import com.wanted.teamr.snsfeedintegration.domain.Member;
 import com.wanted.teamr.snsfeedintegration.dto.MemberApprovalRequest;
 import com.wanted.teamr.snsfeedintegration.exception.CustomException;
 import com.wanted.teamr.snsfeedintegration.exception.ErrorCode;
@@ -12,9 +13,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
+import static com.wanted.teamr.snsfeedintegration.controller.TestConstants.ACCOUNT_NAME;
+import static com.wanted.teamr.snsfeedintegration.controller.TestConstants.PASSWORD;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -27,6 +31,8 @@ class MemberServiceMockTest {
     private MemberService sut;
     @Mock
     private MemberRepository memberRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @DisplayName("사용자 가입승인 서비스 Mock 테스트")
     @Nested
@@ -43,6 +49,32 @@ class MemberServiceMockTest {
             String wrongAccountName = "wrongAccountName";
             given(dto.getAccountName())
                     .willReturn(wrongAccountName);
+
+            // when, then
+            Assertions.assertThatThrownBy(() -> sut.approve(dto))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(ErrorCode.ACCOUNT_INFO_WRONG.getMessage());
+        }
+
+        @DisplayName("[비밀번호] 불일치")
+        @Test
+        void givenWrongPassword_thenThrowsWithACCOUNT_INFO_WRONG() {
+            // given
+            Member member = mock(Member.class);
+            given(memberRepository.findByAccountName(anyString()))
+                    .willReturn(Optional.of(member));
+
+            MemberApprovalRequest dto = mock(MemberApprovalRequest.class);
+            given(dto.getAccountName())
+                    .willReturn(ACCOUNT_NAME);
+
+            given(member.getPassword())
+                    .willReturn(PASSWORD);
+            given(dto.getPassword())
+                    .willReturn(PASSWORD);
+
+            given(passwordEncoder.matches(dto.getPassword(), member.getPassword()))
+                    .willReturn(false);
 
             // when, then
             Assertions.assertThatThrownBy(() -> sut.approve(dto))
