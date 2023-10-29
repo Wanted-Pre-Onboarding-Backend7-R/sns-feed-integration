47b337eef0a0648e009f425db267d8e10cadcefa
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-29 17:42:10 +0900

    test #22: (통합) 가입승인 시 계정이름 찾을 수 없으면 예외

--- src/test/java/com/wanted/teamr/snsfeedintegration/service/MemberServiceTest.java
+++ src/test/java/com/wanted/teamr/snsfeedintegration/service/MemberServiceTest.java
@@ -1,12 +1,15 @@
 package com.wanted.teamr.snsfeedintegration.service;
 
 import com.wanted.teamr.snsfeedintegration.domain.Member;
+import com.wanted.teamr.snsfeedintegration.dto.MemberApprovalRequest;
 import com.wanted.teamr.snsfeedintegration.dto.MemberJoinRequest;
 import com.wanted.teamr.snsfeedintegration.exception.CustomException;
 import com.wanted.teamr.snsfeedintegration.exception.ErrorCode;
 import com.wanted.teamr.snsfeedintegration.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -62,6 +65,33 @@ class MemberServiceTest {
             assertThat(passwordEncoder.matches(dto.getPassword(), foundMember.getPassword())).isTrue();
             assertThat(foundMember.getApprovalCode()).hasSize(6);
             assertThat(foundMember.getIsApproved()).isFalse();
+        }
+
+    }
+
+    @DisplayName("사용자 가입승인 서비스")
+    @Order(2)
+    @Nested
+    class Approve {
+
+        private String approvalCode;
+
+        @BeforeEach
+        void setUp() {
+            approvalCode = memberRepository.findById(1L).orElseThrow().getApprovalCode();
+        }
+
+        @DisplayName("[계정 이름] 찾을 수 없음")
+        @ValueSource(strings = {"myaccoun", "myaccount2"})
+        @ParameterizedTest
+        void givenNotFoundAccountName_thenThrowsWithACCOUNT_INFO_WRONG(String accountName) {
+            // given
+            MemberApprovalRequest dto = MemberApprovalRequest.of(accountName, PASSWORD, approvalCode);
+
+            // when, then
+            Assertions.assertThatThrownBy(() -> sut.approve(dto))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(ErrorCode.ACCOUNT_INFO_WRONG.getMessage());
         }
 
     }
