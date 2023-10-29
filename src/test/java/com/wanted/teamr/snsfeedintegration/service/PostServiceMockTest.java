4203f2d55cdcd2b2f5f12789453393849efc217f
mojh7 <mjh79017901@gmail.com>
2023-10-29 19:57:21 +0900

    test #21: 게시물 공유 기능 테스트 작성
     - 게시물 공유 동시성 테스트 100번으로도 충분히 검증되고 테스트 시간 줄이기 위해 1000 -> 100번으로 설정

--- src/test/java/com/wanted/teamr/snsfeedintegration/service/PostServiceMockTest.java
+++ src/test/java/com/wanted/teamr/snsfeedintegration/service/PostServiceMockTest.java
@@ -1,5 +1,6 @@
 package com.wanted.teamr.snsfeedintegration.service;
 
+import com.wanted.teamr.snsfeedintegration.config.ExternalApiConfig;
 import com.wanted.teamr.snsfeedintegration.domain.Post;
 import com.wanted.teamr.snsfeedintegration.domain.PostHashtag;
 import com.wanted.teamr.snsfeedintegration.domain.SnsType;
@@ -20,7 +21,11 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,6 +36,12 @@ class PostServiceMockTest {
 
     @InjectMocks
     PostService postService;
+
+    @Mock
+    SnsService snsService;
+
+    @Mock
+    ExternalApiConfig externalApiConfig;
 
     @DisplayName("게시물 한 건에 대한 상세 정보를 가져온다.")
     @Test
@@ -90,6 +101,55 @@ class PostServiceMockTest {
                 .isInstanceOf(CustomException.class)
                 .extracting("errorCodeType")
                 .isEqualTo(ErrorCode.POST_NOT_FOUND);
+    }
+
+    @DisplayName("게시물 공유 성공")
+    @Test
+    void sharePost() {
+        // given: 게시물이 존재하도록 설정
+        Long postId = 12321L;
+        Long shareCount = 10235L;
+        Post post = Post.builder()
+                        .contentId("1234567")
+                        .type(SnsType.INSTAGRAM)
+                        .title("강아지 졸귀")
+                        .content("우리집 강아지 보고가세요")
+                        .viewCount(225672L)
+                        .likeCount(45333L)
+                        .shareCount(shareCount)
+                        .createdAt(LocalDateTime.of(2023, 8, 10, 8, 5, 22))
+                        .updatedAt(LocalDateTime.of(2023, 8, 13, 17, 35, 42))
+                        .build();
+        given(postRepository.findByIdForUpdate(postId)).willReturn(Optional.of(post));
+        given(snsService.sharePost(post.getContentId(), post.getType())).willReturn(true);
+
+        // when
+        postService.sharePost(postId);
+
+        // then: 공유 수가 +1 됐는지 확인
+        assertAll(
+                () -> verify(postRepository).findByIdForUpdate(postId),
+                () -> verify(snsService).sharePost(post.getContentId(), post.getType()),
+                () -> assertThat(post.getShareCount()).isEqualTo(shareCount + 1)
+        );
+    }
+
+    @DisplayName("게시물을 공유할 때 게시물 id에 해당하는 게시물을 찾을 수 없어 예외가 발생한다.")
+    @Test
+    void sharePostFailedPostNotFound() {
+        // given: postId에 해당하는 게시물이 존재하지 않도록 설정
+        Long postId = 222500L;
+        given(postRepository.findByIdForUpdate(postId)).willReturn(Optional.empty());
+
+        // when
+        CustomException ex = assertThrows(CustomException.class, () -> postService.sharePost(postId));
+
+        // then: POST_NOT_FOUND 예외 발생 확인
+        assertAll(
+                () -> assertThat(ex.getErrorCodeType()).isEqualTo(ErrorCode.POST_NOT_FOUND),
+                () -> verify(postRepository).findByIdForUpdate(postId),
+                () -> verify(snsService, never()).sharePost(any(String.class), any(SnsType.class))
+        );
     }
 
 }
