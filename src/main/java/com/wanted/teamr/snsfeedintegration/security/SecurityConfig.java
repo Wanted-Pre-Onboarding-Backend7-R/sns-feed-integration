7d87a75a0e97a9d0efbf514347c8d03cbd99a190
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-30 17:49:03 +0900

    refactor #30: 시큐리티 빈 설정 수정
    불필요 필드 제거
    `Component`를 제거하고 빈으로 주입
    Jwt 필터 설정 클래스를 `Configuration`으로 등록

--- src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
@@ -2,10 +2,7 @@ package com.wanted.teamr.snsfeedintegration.security;
 
 import com.wanted.teamr.snsfeedintegration.jwt.JwtAccessDeniedHandler;
 import com.wanted.teamr.snsfeedintegration.jwt.JwtAuthenticationEntryPoint;
-import com.wanted.teamr.snsfeedintegration.jwt.TokenProvider;
-import com.wanted.teamr.snsfeedintegration.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,12 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Value("${jwt.secret}")
-    String SECRET_KEY;
-    private final UserDetailsServiceImpl userDetailsService;
-    private final TokenProvider tokenProvider;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtSecurityConfig jwtSecurityConfig;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,11 +27,11 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling((exceptionHandling) ->
                         exceptionHandling
-                                .accessDeniedHandler(jwtAccessDeniedHandler)
-                                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler())
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint())
                 )
                 // enable h2-console
-                .headers((headers)->
+                .headers((headers) ->
                         headers.contentTypeOptions(contentTypeOptionsConfig ->
                                 headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)))
                 // disable session
@@ -47,7 +39,7 @@ public class SecurityConfig {
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
 
-                .authorizeHttpRequests((authorizeRequests)->
+                .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
                                 .requestMatchers("/api/members/**").permitAll()
                                 .requestMatchers(PathRequest.toH2Console()).permitAll()
@@ -55,7 +47,7 @@ public class SecurityConfig {
                 );
 
         /* JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용 */
-        http.apply(new JwtSecurityConfig(SECRET_KEY, tokenProvider, userDetailsService));
+        http.apply(jwtSecurityConfig);
 
         return http.build();
     }
@@ -63,6 +55,16 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public JwtAccessDeniedHandler jwtAccessDeniedHandler() {
+        return new JwtAccessDeniedHandler();
     }
 
 }
