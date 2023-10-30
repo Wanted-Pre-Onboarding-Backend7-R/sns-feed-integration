4039938978024e81fe76b71fe7085aa9f964de44
mojh7 <mjh79017901@gmail.com>
2023-10-30 23:30:43 +0900

    fix #29: Security h2 관련 설정 off, h2 의존성 제거
     - h2 관련된 error 발생해서 제거

--- src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
+++ src/main/java/com/wanted/teamr/snsfeedintegration/security/SecurityConfig.java
@@ -3,13 +3,11 @@ package com.wanted.teamr.snsfeedintegration.security;
 import com.wanted.teamr.snsfeedintegration.jwt.JwtAccessDeniedHandler;
 import com.wanted.teamr.snsfeedintegration.jwt.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,10 +29,6 @@ public class SecurityConfig {
                                 .accessDeniedHandler(jwtAccessDeniedHandler())
                                 .authenticationEntryPoint(jwtAuthenticationEntryPoint())
                 )
-                // enable h2-console
-                .headers((headers) ->
-                        headers.contentTypeOptions(contentTypeOptionsConfig ->
-                                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)))
                 // disable session
                 .sessionManagement((sessionManagement) ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -43,7 +37,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
                                 .requestMatchers("/api/members/**").permitAll()
-                                .requestMatchers(PathRequest.toH2Console()).permitAll()
                                 .anyRequest().authenticated()
                 );
 
